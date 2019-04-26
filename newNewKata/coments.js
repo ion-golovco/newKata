3a666b83e824ff333554f8a0c0516bd3621df18c
ion-golovco <gaminglions2@gmail.com>
2019-04-26 17:47:16 +0300

    Update coments.js

--- newNewKata/coments.js
+++ newNewKata/coments.js
@@ -1,14 +1,14 @@
-function solution(input, markers) {
-    let strings = input.split("\n")
-    for (let i = 0; i < strings.length; i++) {
-        for (let j = 0; j < strings[i].length; j++) {
-            if (strings[i].charAt(j) == markers[0] || strings[i].charAt(j) == markers[1]) {
-                strings[i] = strings[i].slice(0, j - 1)
+function solution(input, m) {
+    let str = input.split("\n")
+    for (let i = 0; i < str.length; i++) {
+        for (let j = 0; j < str[i].length; j++) {
+            if (str[i].charAt(j) == m[0] || str[i].charAt(j) == m[1]) {
+                str[i] = str[i].slice(0, j - 1)
                 console.log(j, i)
             }
         }
     }
-    return strings.join("\n")
+    return str.join("\n")
 };
 console.log(solution("apples, plums % and bananas\npears\noranges !applesauce", ["%", "!"]))
 
