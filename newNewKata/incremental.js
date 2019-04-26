777b4b01b19ce28a75f2c95f4d01baa054434c0b
ion-golovco <gaminglions2@gmail.com>
2019-04-26 17:36:38 +0300

    Add files via upload

--- newNewKata/incremental.js
+++ newNewKata/incremental.js
@@ -1,4 +1,5 @@
 //Genereaza numere si le incrementeaza cu un increment
+
 let number = Math.floor(Math.random()*255)
 let returnul = 0
 let inc = 1
@@ -6,19 +7,33 @@ let index = 0
 function incremental(n,inc){
     returnul = n+inc
 }
+function minusIncremental(n,inc){
+    returnul = n-inc
+}
 main()
 function main(){
+    if(returnul<number){
     while(returnul<=number){
         incremental(returnul,inc)
         console.log([returnul,number])
     }
-    if(number>250){
-        return console.log("gata boss")
+}
+if(returnul>number){
+    while(returnul>number){
+        minusIncremental(returnul,inc)
+        console.log([returnul,number])
     }
+}
+
+    //This is a limit because it does create infinite arrays so it nedds an end
+    if(number>220){
+        return console.log("gata boss")
+
+    }
+
     if(returnul>=number){
         index = index+1
         number = Math.floor(Math.random()*255)
-        returnul = 0
         main()
     }
 }
